d9c8840e143e6af623c91114b394728716825e48
mikaelKM <mikaelkasike861@gmail.com>
2019-10-15 11:06:42 +0300

    added more sql commands

--- loading external data.sql
+++ loading external data.sql
@@ -5,6 +5,7 @@ L_NAME VARCHAR (20) NOT NULL,
 GENDER VARCHAR (6) NOT NULL, 
 STAGE VARCHAR (7) NOT NULL
 );
+
 ALTER TABLE s_details
  ADD ADM_NO VARCHAR(15) NOT NULL UNIQUE AFTER ID;
 
@@ -31,7 +32,7 @@ ESCAPED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES;
 
-SELECT *  FROM s_details;
+SELECT *  FROM student_details;
 
 SELECT *, CONCAT(SUBSTRING(F_NAME,1,1), L_NAME, '@maseno.ac.ke') AS EMAIL FROM s_details;
 
@@ -43,3 +44,22 @@ UPDATE s_details SET EMAIL = CONCAT(SUBSTRING(F_NAME,1,1), L_NAME, '@maseno.ac.k
 SELECT * FROM s_details WHERE SUBSTRING(ADM_NO,10,12)='017' AND (CHAR_LENGTH(F_NAME)>7);
 
 SHOW INDEX FROM s_details;
+
+ALTER TABLE s_details CHANGE email email VARCHAR(100) AFTER gender; /*changing column position*/
+
+ALTER TABLE s_details RENAME STUDENT_DETAILS; /*renaming table*/
+
+CREATE TABLE tudent_details_Backup SELECT * FROM student_details; /*creating a backup tabale*/
+
+CREATE TABLE new_backup LIKE STUDENT_DETAILS; /*creating a new table with the old table structure*/
+SELECT * FROM new_backup;
+DROP TABLE new_backup;
+
+
+SELECT * FROM tudent_details_Backup;
+
+SELECT * INTO new_backup FROM student_details; /*not working */
+
+SELECT COUNT(*) AS student_no FROM student_details WHERE SUBSTRING(ADM_NO,10,12)='017' AND (CHAR_LENGTH(F_NAME)>7);
+SELECT * FROM student_details LIMIT 3; /*selecting limited records*/ 
+SELECT * FROM student_details WHERE RIGHT(adm_no, 3)='017'OR RIGHT(adm_no, 3)='019' LIMIT 3; /*right function takes the characters in the right side, while left vice versa */
